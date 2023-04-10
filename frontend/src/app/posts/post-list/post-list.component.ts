72a2afb31ad80dfc6752f1c4d1f917b5bd2f5e43
akshaya <akshaya.sivakumar@qburst.com>
2023-04-10 15:00:37 +0530

    created post model

--- frontend/src/app/posts/post-list/post-list.component.ts
+++ frontend/src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Post } from '../post.model'
 
 @Component({
   selector: 'app-post-list',
@@ -14,6 +15,6 @@ export class PostListComponent {
   //   {title: 'Fourth post', content: 'this is the fourth post\'s content'}
   // ]
 
-  @Input() posts: any [] = [];
+  @Input() posts: Post [] = [];
 
 }
